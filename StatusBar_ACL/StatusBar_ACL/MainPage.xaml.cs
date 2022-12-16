a5c472a08cb3f6cfc6c03372f63fee7140b8820e
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-12-16 12:43:28 -0600

    first commit

---
+++ StatusBar_ACL/StatusBar_ACL/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using StatusBar_ACL.VistaModelo;
+
+namespace StatusBar_ACL
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new VMmainpage();
+        }
+    }
+}
